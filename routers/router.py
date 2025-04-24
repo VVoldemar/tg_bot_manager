b54f6ce6e51e6a55430464466b446b7842c5bced
VVoldemar <vvd0608@mail.ru>
2025-04-24 17:28:32 +0300

    feat: app button callback, referral

--- routers/router.py
+++ routers/router.py
@@ -6,18 +6,27 @@ from aiogram import types
 
 router = Router()
 
-@router.message(filters.CommandStart(deep_link=True))
+@router.message(filters.CommandStart())
 async def start(message: types.Message, command: filters.CommandObject):
     print(command.args)
     
     buttons = [
-        [types.KeyboardButton(text=strings.KEYBOARD.PROFILE), types.KeyboardButton(text=strings.KEYBOARD.BUY)],
-        [types.KeyboardButton(text=strings.KEYBOARD.HELP), types.KeyboardButton(text=strings.KEYBOARD.REFERRAL)]
+        [
+            types.KeyboardButton(text=strings.KEYBOARD.PROFILE),
+            types.KeyboardButton(text=strings.KEYBOARD.BUY)
+        ],
+        [
+            types.KeyboardButton(text=strings.KEYBOARD.HELP),
+            types.KeyboardButton(text=strings.KEYBOARD.REFERRAL)
+        ]
     ]
     if True:  # TODO: check subscription
         buttons.insert(
             1,
-            [types.KeyboardButton(text=strings.KEYBOARD.APP), types.KeyboardButton(text=strings.KEYBOARD.CONFIG)],
+            [
+                types.KeyboardButton(text=strings.KEYBOARD.APP), types.KeyboardButton(
+                    text=strings.KEYBOARD.CONFIG)
+            ],
         )
     await message.answer(
         strings.GREETING,
@@ -35,10 +44,46 @@ async def message(message: types.Message):
         case strings.KEYBOARD.BUY:
             await message.answer("Тут должно быть получение...")
         case strings.KEYBOARD.APP:
-            await message.answer(strings.APP)
+            await message.answer(
+                strings.APP.MESSAGE,
+                reply_markup=types.InlineKeyboardMarkup(
+                    inline_keyboard=[
+                        [
+                            types.InlineKeyboardButton(
+                                text="🤖 Android", callback_data=strings.APP.CALLBACK.ANDROID),
+                            types.InlineKeyboardButton(
+                                text="📱 IOS", callback_data=strings.APP.CALLBACK.IOS)
+                        ],
+                        [
+                            types.InlineKeyboardButton(
+                                text="🖥️ Windows", callback_data=strings.APP.CALLBACK.WINDOWS),
+                            types.InlineKeyboardButton(
+                                text="💻 MacOS", callback_data=strings.APP.CALLBACK.MACOS)
+                        ],
+                        [types.InlineKeyboardButton(
+                            text="⚙️ Other",
+                            callback_data=strings.APP.CALLBACK.OTHER
+                        )]
+                    ]
+                )
+            )
         case strings.KEYBOARD.CONFIG:
             await message.answer("Тут должен спавница конфиг...")
         case strings.KEYBOARD.HELP:
             await message.answer(strings.HELP)
         case strings.KEYBOARD.REFERRAL:
-            await message.answer(strings.REFERRAL)
+            await message.answer(strings.REFERRAL(message.from_user.id))
+
+
+@router.callback_query()
+async def callback(query: types.CallbackQuery):
+    if query.data in strings.APP.CALLBACK.__dict__.values():
+        await query.message.answer(
+            getattr(strings.APP.ANSWER, query.data.upper()),
+            reply_markup=types.InlineKeyboardMarkup(
+                inline_keyboard=[[types.InlineKeyboardButton(
+                    text=strings.APP.OPEN, 
+                    url=getattr(strings.APP.LINKS, query.data.upper())
+                )]]
+            )
+        )
--- strings.py
+++ strings.py
@@ -1,6 +1,6 @@
 GREETING = '''
 VPN
-'''
+'''   # TODO
 
 def PROFILE(name, subscription, expiration_date):
     return f'''
@@ -11,13 +11,43 @@ def PROFILE(name, subscription, expiration_date):
 
 
 APP = '''
-'''
+'''  # TODO
 
 HELP = '''
+'''  # TODO
+
+def REFERRAL(id):  # TODO
+    return f'''
+https://t.me/your_bot_name?start={id}
 '''
 
-REFERRAL = '''
-'''
+class APP:
+    MESSAGE = '''
+None
+''' # TODO
+    class CALLBACK:
+        ANDROID = "android"
+        IOS = "ios"
+        WINDOWS = "windows"
+        MACOS = "macos"
+        OTHER = "other"
+
+    class ANSWER: # TODO
+        ANDROID = "android"
+        IOS = "ios"
+        WINDOWS = "windows"
+        MACOS = "macos"
+        OTHER = "other"
+
+    class LINKS:
+        ANDROID = "https://play.google.com/store/apps/details?id=com.wireguard.android"
+        IOS = "https://itunes.apple.com/us/app/wireguard/id1441195209?ls=1&mt=8"
+        WINDOWS = "https://download.wireguard.com/windows-client/wireguard-installer.exe"
+        MACOS = "https://itunes.apple.com/us/app/wireguard/id1451685025?ls=1&mt=12"
+        OTHER = "https://www.wireguard.com/install/"
+
+    OPEN = "🔗 Скачать"
+
 
 class KEYBOARD:
     PROFILE = "👤 Профиль"
